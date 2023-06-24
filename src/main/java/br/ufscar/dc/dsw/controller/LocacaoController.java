34829c2342c9d4a8d177fa94d591f5ae4e11cb99
brunomatosdesouza <brunomatos@estudante.ufscar.br>
2023-06-24 18:30:14 -0300

    .

--- src/main/java/br/ufscar/dc/dsw/controller/LocacaoController.java
+++ src/main/java/br/ufscar/dc/dsw/controller/LocacaoController.java
@@ -15,7 +15,62 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.ufscar.dc.dsw.util.Erro;
+import br.ufscar.dc.dsw.dao.LocacaoDAO;
+import br.ufscar.dc.dsw.domain.Usuario;
 
-public class LocacaoController {
-    
+@WebServlet(urlPatterns = "/locacoes/*")
+public class LocacaoController extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    private LocacaoDAO dao;
+
+    @Override
+    public void init() {
+        dao = new LocacaoDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Usuario usuario = (Usuario) request.getSession().getAttribute("usuarioLogado");
+		Erro erros = new Erro();
+
+		if (usuario == null) {
+			response.sendRedirect(request.getContextPath());
+			return;
+		} else if (!usuario.getPapel().equals("USER")) {
+			erros.add("Acesso não autorizado!");
+			erros.add("Apenas Papel [USER] tem acesso a essa página");
+			request.setAttribute("mensagens", erros);
+			RequestDispatcher rd = request.getRequestDispatcher("/noAuth.jsp");
+			rd.forward(request, response);
+			return;
+		}
+		
+        String action = request.getPathInfo();
+        if (action == null) {
+            action = "";
+        }
+        /* 
+        try {
+            switch (action) {
+                case "/cadastro":
+                    apresentaFormCadastro(request, response);
+                    break;
+                case "/insercao":
+                    insere(request, response);
+                    break;
+                default:
+                    lista(request, response);
+                    break;
+            }
+        } catch (RuntimeException | IOException | ServletException e) {
+            throw new ServletException(e);
+        }*/
+    }
 }
